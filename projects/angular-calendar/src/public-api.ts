1d08d238e8c2ea8da275785be225c1e68764258d
Maxim Milovanov <mma29121983@gmail.com>
2020-01-13 22:13:42 -0500

    removes useless code

---
+++ projects/angular-calendar/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of angular-calendar
+ */
+
+export { CalendarUtils } from './modules/common/calendar-utils.provider';
+export * from './modules/common/calendar-common.module';
+export * from './modules/week/calendar-week.module';
+
+export { CalendarWeekModule } from './modules/week/calendar-week.module';
--- projects/demos/app/demo-app.module.ts
+++ projects/demos/app/demo-app.module.ts
@@ -1,57 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  NgbTabsetModule,
-  NgbCollapseModule,
-  NgbTooltipModule
-} from '@ng-bootstrap/ng-bootstrap';
-import { Angulartics2Module } from 'angulartics2';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { DragAndDropModule } from 'angular-draggable-droppable';
 import { DemoAppComponent } from './demo-app.component';
-import { environment } from '../environments/environment';
-import { FormsModule } from '@angular/forms';
-import { ClipboardModule } from 'ngx-clipboard';
+import { CalendarWeekModule } from 'angular-calendar';
 
 @NgModule({
   declarations: [DemoAppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    NgbTabsetModule,
-    NgbCollapseModule,
-    NgbTooltipModule,
-    DragAndDropModule,
-    Angulartics2Module.forRoot({
-      developerMode: !environment.production
-    }),
-    PerfectScrollbarModule,
-    ClipboardModule,
-    RouterModule.forRoot(
-      [
-        {
-          path: 'week-view-minute-precision',
-          loadChildren: () =>
-            import('./demo-modules/week-view-minute-precision/module').then(
-              m => m.DemoModule
-            ),
-          data: {
-            label: 'Week view minute precision'
-          }
-        },
-        {
-          path: '**',
-          redirectTo: 'week-view-minute-precision'
-        }
-      ],
-      {
-        useHash: true
-      }
-    )
-  ],
+  imports: [BrowserModule, CalendarWeekModule],
   bootstrap: [DemoAppComponent]
 })
 export class DemoAppModule {}
